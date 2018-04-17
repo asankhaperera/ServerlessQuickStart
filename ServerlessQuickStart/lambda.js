390ad097638eea84d0b4f617a266873c79b3a125
asankhaperera <36698455+asankhaperera@users.noreply.github.com>
2018-04-17 22:35:35 +0530

    initial commit

---
+++ ServerlessQuickStart/lambda.js
@@ -0,0 +1,19 @@
+let AWS = require('aws-sdk');
+exports.handler = function(event, context, callback) {
+    
+	const firstNumber = Number(event.queryStringParameters.number1); 
+    const secondNumber = Number(event.queryStringParameters.number2); 
+    const result = firstNumber + secondNumber; 
+
+    var responseBody = { 
+        "result": result 
+    }; 
+
+    var response = { 
+        "statusCode": 200, 
+        "body": JSON.stringify(responseBody), 
+        "isBase64Encoded": false 
+    }; 
+
+    callback(null, response); 
+}
