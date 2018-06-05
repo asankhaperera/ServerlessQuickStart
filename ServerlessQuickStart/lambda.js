6a9738e4a4797d3970692f7ecd97ab3549fbda8f
asankhaperera <36698455+asankhaperera@users.noreply.github.com>
2018-06-05 08:50:49 +0530

    update

--- ServerlessQuickStart/lambda.js
+++ ServerlessQuickStart/lambda.js
@@ -1,21 +1,25 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
-    
-	const firstNumber = Number(event.queryStringParameters.number1); 
-    const secondNumber = Number(event.queryStringParameters.number2); 
-    const result = firstNumber + secondNumber; 
+const s3 = new AWS.S3();
+exports.handler = function (event, context, callback) {
 
-    var responseBody = { 
-        "result": result 
-    }; 
+    const firstNumber = Number(event.queryStringParameters.number1);
+    const secondNumber = Number(event.queryStringParameters.number2);
+    const result = firstNumber + secondNumber;
 
-    console.log('Request : ' + firstNumber + ' + ' + secondNumber + ' and Result = ' + result);
+    var responseBody = {
+        "result": result
+    };
 
-    var response = { 
-        "statusCode": 200, 
-        "body": JSON.stringify(responseBody), 
-        "isBase64Encoded": false 
-    }; 
+    console.log('Request : ' + firstNumber + ' + ' + secondNumber + ' and innovfest Result = ' + result);
 
-    callback(null, response); 
+    var response = {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*"
+        },
+        "body": JSON.stringify(responseBody),
+        "isBase64Encoded": false
+    };
+
+    callback(null, response);
 }
